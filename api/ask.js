fc17f6ba4b7c94c3bda8e0a9fbb21243c28e7984
sai-1973 <151039085+sai-1973@users.noreply.github.com>
2026-04-13 19:23:14 +0530

    Fix Vercel routing and module export

--- api/ask.js
+++ api/ask.js
@@ -1,6 +1,6 @@
 const fetch = require("node-fetch");
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
